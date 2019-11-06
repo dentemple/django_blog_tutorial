92ac9cded8a6f11fbddddd38e1aec365a06448ee
Denny S. Temple <dev@dentemple.com>
2019-11-06 11:47:37 -0500

    Handle models

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -4,25 +4,28 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-    {
-        "author": "Kurtz",
-        "title": "Blog Post 1",
-        "content": "First post content",
-        "date_posted": "Yesterday",
-    },
-    {
-        "author": "Marlow",
-        "title": "Blog Post 2",
-        "content": "First post content",
-        "date_posted": "Today",
-    },
-]
+from .models import Post
+
+# posts = [
+#     {
+#         "author": "Kurtz",
+#         "title": "Blog Post 1",
+#         "content": "First post content",
+#         "date_posted": "Yesterday",
+#     },
+#     {
+#         "author": "Marlow",
+#         "title": "Blog Post 2",
+#         "content": "First post content",
+#         "date_posted": "Today",
+#     },
+# ]
 
 
 def home(request):
     # return HttpResponse("<h1>Blog Home</h1>")
-    context = {"posts": posts}
+    # context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
